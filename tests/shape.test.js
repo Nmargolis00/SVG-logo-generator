fae401d3846076361e52b461f63af01b6a5ca2fc
Nmargolis00 <119348315+Nmargolis00@users.noreply.github.com>
2023-03-06 12:49:12 -0700

    all tests work

--- tests/shape.test.js
+++ tests/shape.test.js
@@ -16,3 +16,25 @@ describe('Circle', () => {
 
 
 //Square Test
+describe('Square', () => {
+    test("should render svg for a green square element", () => {
+        const expectedSvg = `<rect x="90" y="40" width="120" height="120" fill="green" />`;
+        const square = new Square();
+        square.setColor("green");
+        const actualSvg = square.render();
+        console.log(actualSvg, expectedSvg);
+        expect(actualSvg).toEqual(expectedSvg);
+    });
+});
+
+//Triangle Test
+describe('Triangle', () => {
+    test("should render svg for a green triangle element", () => {
+        const expectedSvg = `<polygon points="150, 18 244, 182 56, 182" fill="green" />`;
+        const triangle = new Triangle();
+        triangle.setColor("green");
+        const actualSvg = triangle.render();
+        console.log(actualSvg, expectedSvg);
+        expect(actualSvg).toEqual(expectedSvg);
+    });
+});
