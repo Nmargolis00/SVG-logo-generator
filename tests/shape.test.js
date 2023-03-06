d6a6b4f8e86973c32042458afe99f4f6a8f02561
Nmargolis00 <119348315+Nmargolis00@users.noreply.github.com>
2023-03-06 12:02:05 -0700

    added more tests

--- tests/shape.test.js
+++ tests/shape.test.js
@@ -1,36 +1,37 @@
 const {Circle, Square, Triangle} = require('../lib/shapes');
-const index = require('index');
 
 
 //Circle Test
-describe('Is a Circle a Circle', () => {
-    it('Should throw an error if circle !=== circle', () => {
-        const circle =  new Circle();
-        const err = new Error('I do not know what shape this is but it is not a circle')
-        expect(circle).toEqual({
-            shape: 'Circle'
-        })
-    })
+describe('Circle', () => {
+    test("should render svg for a green circle element", () => {
+        const expectedSvg = '<circle cx="150" cy="100" r="80" fill="green" />';
+        const circle = new Circle();
+        circle.setColor("green");
+        const actualSvg = circle.render();
+        expect(actualSvg).toEqual(expectedSvg);
+    });
 });
+
+  // Make the square and triangle test match
 
 //Square Test
-describe('Is a Square a Square', () => {
-    it('Should throw an error if square !=== square', () => {
-        const square =  new Square();
-        const err = new Error('I do not know what shape this is but it is not a square')
-        expect(square).toEqual({
-            shape: 'Square'
-        })
-    })
-});
+// describe('Is a Square a Square', () => {
+//     it('Should throw an error if square !=== square', () => {
+//         const square =  new Square();
+//         const err = new Error('I do not know what shape this is but it is not a square')
+//         expect(square).toEqual({
+//             shape: 'Square'
+//         })
+//     })
+// });
 
-//Triangle Test
-describe('Is a Triangle a Triangle', () => {
-    it('Should throw an error if triangle !=== triangle', () => {
-        const triangle =  new Triangle();
-        const err = new Error('I do not know what shape this is but it is not a triangle')
-        expect(triangle).toEqual({
-            shape: 'Triangle'
-        })
-    })
-});
+// //Triangle Test
+// describe('Is a Triangle a Triangle', () => {
+//     it('Should throw an error if triangle !=== triangle', () => {
+//         const triangle =  new Triangle();
+//         const err = new Error('I do not know what shape this is but it is not a triangle')
+//         expect(triangle).toEqual({
+//             shape: 'Triangle'
+//         })
+//     })
+// });
--- tests/svg.test.js
+++ tests/svg.test.js
@@ -1,2 +1,14 @@
 const SVG = require('../lib/svg');
-const index = require("index");
+
+test('Can it render an SVG', () => {
+    const svg = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"></svg>';
+    const newSVG = new SVG();
+    expect(newSVG.render()).toEqual(svg);
+}) 
+
+// test('Can it write text', () => {
+//     const svgText = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">A</text</svg>'
+//     const newSVG = new SVG ();
+//     newSVG.setTextColor('A', "white")
+//     expect(newSVG.render()).toEqual(svgText);
+// })
