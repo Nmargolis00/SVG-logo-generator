4ae1afca9447eb3748cc694747c1f62e50731e92
Nmargolis00 <119348315+Nmargolis00@users.noreply.github.com>
2023-03-06 12:46:20 -0700

    got test to work

--- tests/shape.test.js
+++ tests/shape.test.js
@@ -4,34 +4,15 @@ const {Circle, Square, Triangle} = require('../lib/shapes');
 //Circle Test
 describe('Circle', () => {
     test("should render svg for a green circle element", () => {
-        const expectedSvg = '<circle cx="150" cy="100" r="80" fill="green" />';
+        const expectedSvg = `<circle cx="150" cy="100" r="80" fill="green" />`;
         const circle = new Circle();
         circle.setColor("green");
         const actualSvg = circle.render();
+        console.log(actualSvg, expectedSvg);
         expect(actualSvg).toEqual(expectedSvg);
     });
 });
 
-  // Make the square and triangle test match
+
 
 //Square Test
-// describe('Is a Square a Square', () => {
-//     it('Should throw an error if square !=== square', () => {
-//         const square =  new Square();
-//         const err = new Error('I do not know what shape this is but it is not a square')
-//         expect(square).toEqual({
-//             shape: 'Square'
-//         })
-//     })
-// });
-
-// //Triangle Test
-// describe('Is a Triangle a Triangle', () => {
-//     it('Should throw an error if triangle !=== triangle', () => {
-//         const triangle =  new Triangle();
-//         const err = new Error('I do not know what shape this is but it is not a triangle')
-//         expect(triangle).toEqual({
-//             shape: 'Triangle'
-//         })
-//     })
-// });
